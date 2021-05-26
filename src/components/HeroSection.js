8c776dbac18ab20691f25b5f90b2f30b67e7a209
Jack Parsons <65678430+jackparse14@users.noreply.github.com>
2021-05-26 02:38:10 +0100

    Name Formatting

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,15 +6,18 @@ import backgroundVideo from "../videos/background-video.mp4";
 function HeroSection() {
     return (
         <div className="hero-container">
-            
             <video src={backgroundVideo} autoPlay loop muted />
-            <h1>Jack Parsons</h1>
-            <p>Video Game Developer</p>
-            <div class="hero-btns-social-media">
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jackparse14"><i class="fab fa-github"/></a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC0kZIFCOeBjJhGrxG5K8hjw"><i class="fab fa-youtube"/></a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jackparsecompsci/"><i class="fab fa-instagram"/></a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/jackparse14"><i class="fab fa-twitter"/></a>
+            <div className="hero-info">
+                <h1>Jack Parsons</h1>
+                <div className="hero-info-secondary">
+                    <p>Video Game Developer</p>
+                    <div className="hero-social-media">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/jackparse14"><i class="fab fa-github"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UC0kZIFCOeBjJhGrxG5K8hjw"><i class="fab fa-youtube"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jackparsecompsci/"><i class="fab fa-instagram"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/jackparse14"><i class="fab fa-twitter"/></a>
+                    </div>
+                </div>
             </div>
         </div>
     )
